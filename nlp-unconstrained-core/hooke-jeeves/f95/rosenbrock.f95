93adbb490b5149ab61bfa6dc5c134c7e151feb84
Radislav (Radic) Golubtsov <rgolubtsov@mail.ru>
2015-05-04 23:55:01 +0300

    NLPUC: Hooke-Jeeves-f95: Code prettify: adjust alignment, separate semantic blocks.

--- nlp-unconstrained-core/hooke-jeeves/f95/rosenbrock.f95
+++ nlp-unconstrained-core/hooke-jeeves/f95/rosenbrock.f95
@@ -165,22 +165,26 @@ module rosenbrock
     interface
         double precision function f(x, n)
             double precision, intent(in) :: x(:)
+
             integer, intent(inout), optional :: n
         end function f
 
         double precision function best_nearby(delta, point, prevbest, nvars)
             double precision, intent(inout) :: delta(:)
             double precision, intent(inout) :: point(:)
-            double precision, intent(in) :: prevbest
+            double precision, intent(in)    :: prevbest
+
             integer, intent(in) :: nvars
         end function best_nearby
 
         integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
             integer, intent(in) :: nvars
-            double precision, intent(in) :: startpt(:)
+
+            double precision, intent(in)  :: startpt(:)
             double precision, intent(out) :: endpt(:)
-            double precision, intent(in) :: rho
-            double precision, intent(in) :: epsilon
+            double precision, intent(in)  :: rho
+            double precision, intent(in)  :: epsilon
+
             integer, intent(in) :: itermax
         end function hooke
     end interface
@@ -189,9 +193,11 @@ end module rosenbrock
 ! Rosenbrock's classic parabolic valley ("banana") function.
 double precision function f(x, n)
     use rosenbrock, only: funevals
+
     implicit none
 
     double precision, intent(in) :: x(:)
+
     integer, intent(inout), optional :: n
 
     double precision :: a
@@ -199,8 +205,10 @@ double precision function f(x, n)
     double precision :: c
 
     funevals = funevals + 1
+
     a = x(1)
     b = x(2)
+
     c = 100.0 * (b - (a * a)) * (b - (a * a))
 
     f = c + ((1.0 - a) * (1.0 - a))
@@ -209,16 +217,19 @@ end function f
 ! Given a point, look for a better one nearby, one coord at a time.
 double precision function best_nearby(delta, point, prevbest, nvars)
     use rosenbrock, only: VARS, f
+
     implicit none
 
     double precision, intent(inout) :: delta(:)
     double precision, intent(inout) :: point(:)
-    double precision, intent(in) :: prevbest
+    double precision, intent(in)    :: prevbest
+
     integer, intent(in) :: nvars
 
     double precision :: minf
     double precision :: z(VARS)
     double precision :: ftmp
+
     integer :: i
 
     minf = prevbest
@@ -229,13 +240,15 @@ double precision function best_nearby(delta, point, prevbest, nvars)
 
     do i = 1, nvars
         z(i) = point(i) + delta(i)
+
         ftmp = f(z)
 
         if (ftmp < minf) then
             minf = ftmp
         else
             delta(i) = 0.0 - delta(i)
-            z(i) = point(i) + delta(i)
+            z(i)     = point(i) + delta(i)
+
             ftmp = f(z)
 
             if (ftmp < minf) then
@@ -255,13 +268,16 @@ end function best_nearby
 
 integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
     use rosenbrock, only: VARS, funevals, f, best_nearby
+
     implicit none
 
     integer, intent(in) :: nvars
-    double precision, intent(in) :: startpt(:)
+
+    double precision, intent(in)  :: startpt(:)
     double precision, intent(out) :: endpt(:)
-    double precision, intent(in) :: rho
-    double precision, intent(in) :: epsilon
+    double precision, intent(in)  :: rho
+    double precision, intent(in)  :: epsilon
+
     integer, intent(in) :: itermax
 
     integer :: i
@@ -269,6 +285,7 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
     integer :: iters
     integer :: j
     integer :: keep
+
     double precision :: newx(VARS)
     double precision :: xbefore(VARS)
     double precision :: delta(VARS)
@@ -279,7 +296,8 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
 
     do i = 1, nvars
         xbefore(i) = startpt(i)
-        newx(i) = xbefore(i)
+        newx(i)    = xbefore(i)
+
         delta(i) = abs(startpt(i) * rho)
 
         if (delta(i) == 0.0) then
@@ -287,18 +305,21 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
         end if
     end do
 
-    iadj = 0
+    iadj       = 0
     steplength = rho
-    iters = 0
+    iters      = 0
+
     fbefore = f(newx)
+
     newf = fbefore
 
     do while ((iters < itermax) .and. (steplength > epsilon))
         iters = iters + 1
-        iadj = iadj + 1
-        write (*, &
-            '(/, "After ", i5, " funevals, f(x) =  ", 1pe11.4e3, " at")') &
-            funevals, fbefore
+        iadj  = iadj + 1
+
+        write ( &
+            *, '(/, "After ", i5, " funevals, f(x) =  ", 1pe11.4e3, " at")' &
+        ) funevals, fbefore
 
         do j = 1, nvars
             write (*, '("   x[", i2, "] = ", 1pe12.4e3)') j - 1, xbefore(j)
@@ -326,12 +347,13 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
                 end if
 
                 ! Now, move further in this direction.
-                tmp = xbefore(i)
+                tmp        = xbefore(i)
                 xbefore(i) = newx(i)
-                newx(i) = newx(i) + newx(i) - tmp
+                newx(i)    = newx(i) + newx(i) - tmp
             end do
 
             fbefore = newf
+
             newf = best_nearby(delta, newx, fbefore, nvars)
 
             ! If the further (optimistic) move was bad....
@@ -373,25 +395,29 @@ end function hooke
 
 program banana
     use rosenbrock, only: VARS, RHO_BEGIN, EPSMIN, IMAX, hooke
+
     implicit none
 
     integer :: nvars
     integer :: itermax
     integer :: jj
     integer :: i
+
     double precision :: startpt(VARS)
     double precision :: rho
     double precision :: epsilon
     double precision :: endpt(VARS)
 
     ! Starting guess for Rosenbrock's test function.
-    nvars = 2
+    nvars      = 2
     startpt(1) = -1.2
     startpt(2) = 1.0
-    itermax = IMAX
-    rho = RHO_BEGIN
-    epsilon = EPSMIN
+    itermax    = IMAX
+    rho        = RHO_BEGIN
+    epsilon    = EPSMIN
+
     jj = hooke(nvars, startpt, endpt, rho, epsilon, itermax)
+
     write (*, '(///, "HOOKE USED ", i2, " ITERATIONS, AND RETURNED")') jj
 
     do i = 1, nvars
--- nlp-unconstrained-core/hooke-jeeves/f95/woods.f95
+++ nlp-unconstrained-core/hooke-jeeves/f95/woods.f95
@@ -36,22 +36,26 @@ module woods
     interface
         double precision function f(x, n)
             double precision, intent(in) :: x(:)
+
             integer, intent(inout), optional :: n
         end function f
 
         double precision function best_nearby(delta, point, prevbest, nvars)
             double precision, intent(inout) :: delta(:)
             double precision, intent(inout) :: point(:)
-            double precision, intent(in) :: prevbest
+            double precision, intent(in)    :: prevbest
+
             integer, intent(in) :: nvars
         end function best_nearby
 
         integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
             integer, intent(in) :: nvars
-            double precision, intent(in) :: startpt(:)
+
+            double precision, intent(in)  :: startpt(:)
             double precision, intent(out) :: endpt(:)
-            double precision, intent(in) :: rho
-            double precision, intent(in) :: epsilon
+            double precision, intent(in)  :: rho
+            double precision, intent(in)  :: epsilon
+
             integer, intent(in) :: itermax
         end function hooke
     end interface
@@ -60,21 +64,24 @@ end module woods
 ! Woods -- a la More, Garbow & Hillstrom (TOMS algorithm 566).
 double precision function f(x, n)
     use woods, only: funevals
+
     implicit none
 
     double precision, intent(in) :: x(:)
+
     integer, intent(inout), optional :: n
 
-    double precision s1
-    double precision s2
-    double precision s3
-    double precision t1
-    double precision t2
-    double precision t3
-    double precision t4
-    double precision t5
+    double precision :: s1
+    double precision :: s2
+    double precision :: s3
+    double precision :: t1
+    double precision :: t2
+    double precision :: t3
+    double precision :: t4
+    double precision :: t5
 
     funevals = funevals + 1
+
     s1 = x(2) - x(1) * x(1)
     s2 = 1 - x(1)
     s3 = x(2) - 1
@@ -84,23 +91,27 @@ double precision function f(x, n)
     t4 = s3 + t3
     t5 = s3 - t3
 
-    f = 100 * (s1 * s1) + s2 * s2 + 90 * (t1 * t1) + t2 * t2 + 10 * (t4 * t4) &
-        + t5 * t5 / 10.
+    f = 100 * (s1 * s1) + s2 * s2 &
+       + 90 * (t1 * t1) + t2 * t2 &
+       + 10 * (t4 * t4) + t5 * t5 / 10.
 end function f
 
 ! Given a point, look for a better one nearby, one coord at a time.
 double precision function best_nearby(delta, point, prevbest, nvars)
     use woods, only: VARS, f
+
     implicit none
 
     double precision, intent(inout) :: delta(:)
     double precision, intent(inout) :: point(:)
-    double precision, intent(in) :: prevbest
+    double precision, intent(in)    :: prevbest
+
     integer, intent(in) :: nvars
 
     double precision :: minf
     double precision :: z(VARS)
     double precision :: ftmp
+
     integer :: i
 
     minf = prevbest
@@ -111,13 +122,15 @@ double precision function best_nearby(delta, point, prevbest, nvars)
 
     do i = 1, nvars
         z(i) = point(i) + delta(i)
+
         ftmp = f(z)
 
         if (ftmp < minf) then
             minf = ftmp
         else
             delta(i) = 0.0 - delta(i)
-            z(i) = point(i) + delta(i)
+            z(i)     = point(i) + delta(i)
+
             ftmp = f(z)
 
             if (ftmp < minf) then
@@ -137,13 +150,16 @@ end function best_nearby
 
 integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
     use woods, only: VARS, funevals, f, best_nearby
+
     implicit none
 
     integer, intent(in) :: nvars
-    double precision, intent(in) :: startpt(:)
+
+    double precision, intent(in)  :: startpt(:)
     double precision, intent(out) :: endpt(:)
-    double precision, intent(in) :: rho
-    double precision, intent(in) :: epsilon
+    double precision, intent(in)  :: rho
+    double precision, intent(in)  :: epsilon
+
     integer, intent(in) :: itermax
 
     integer :: i
@@ -151,6 +167,7 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
     integer :: iters
     integer :: j
     integer :: keep
+
     double precision :: newx(VARS)
     double precision :: xbefore(VARS)
     double precision :: delta(VARS)
@@ -161,7 +178,8 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
 
     do i = 1, nvars
         xbefore(i) = startpt(i)
-        newx(i) = xbefore(i)
+        newx(i)    = xbefore(i)
+
         delta(i) = abs(startpt(i) * rho)
 
         if (delta(i) == 0.0) then
@@ -169,18 +187,21 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
         end if
     end do
 
-    iadj = 0
+    iadj       = 0
     steplength = rho
-    iters = 0
+    iters      = 0
+
     fbefore = f(newx)
+
     newf = fbefore
 
     do while ((iters < itermax) .and. (steplength > epsilon))
         iters = iters + 1
-        iadj = iadj + 1
-        write (*, &
-            '(/, "After ", i5, " funevals, f(x) =  ", 1pe11.4e3, " at")') &
-            funevals, fbefore
+        iadj  = iadj + 1
+
+        write ( &
+            *, '(/, "After ", i5, " funevals, f(x) =  ", 1pe11.4e3, " at")' &
+        ) funevals, fbefore
 
         do j = 1, nvars
             write (*, '("   x[", i2, "] = ", 1pe12.4e3)') j - 1, xbefore(j)
@@ -208,12 +229,13 @@ integer function hooke(nvars, startpt, endpt, rho, epsilon, itermax)
                 end if
 
                 ! Now, move further in this direction.
-                tmp = xbefore(i)
+                tmp        = xbefore(i)
                 xbefore(i) = newx(i)
-                newx(i) = newx(i) + newx(i) - tmp
+                newx(i)    = newx(i) + newx(i) - tmp
             end do
 
             fbefore = newf
+
             newf = best_nearby(delta, newx, fbefore, nvars)
 
             ! If the further (optimistic) move was bad....
@@ -255,27 +277,31 @@ end function hooke
 
 program more_garbow_hillstrom
     use woods, only: VARS, RHO_WOODS, EPSMIN, IMAX, hooke
+
     implicit none
 
     integer :: nvars
     integer :: itermax
     integer :: jj
     integer :: i
+
     double precision :: startpt(VARS)
     double precision :: rho
     double precision :: epsilon
     double precision :: endpt(VARS)
 
     ! Starting guess test problem "Woods".
-    nvars = 4
+    nvars      = 4
     startpt(1) = -3
     startpt(2) = -1
     startpt(3) = -3
     startpt(4) = -1
-    itermax = IMAX
-    rho = RHO_WOODS
-    epsilon = EPSMIN
+    itermax    = IMAX
+    rho        = RHO_WOODS
+    epsilon    = EPSMIN
+
     jj = hooke(nvars, startpt, endpt, rho, epsilon, itermax)
+
     write (*, '(///, "HOOKE USED ", i2, " ITERATIONS, AND RETURNED")') jj
 
     do i = 1, nvars
